e845c5ea1ea3d1eec0ec1bd3d1dbfe109d056805
GB2G <kev2005@outlook.com>
2022-06-12 22:34:21 -0400

    ask monsieur why prefs wont work

--- src/main/java/edu/kje/address/MainApp.java
+++ src/main/java/edu/kje/address/MainApp.java
@@ -11,7 +11,9 @@ import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 import edu.kje.address.model.Person;
 import edu.kje.address.view.PersonEditDialogController;
@@ -138,6 +140,44 @@ public class MainApp extends Application {
             return false;
         }
     }
+
+    /**
+ * Returns the person file preference, i.e. the file that was last opened.
+ * The preference is read from the OS specific registry. If no such
+ * preference can be found, null is returned.
+ * 
+ * @return
+ */
+public File getPersonFilePath() {
+    Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+    String filePath = prefs.get("filePath", null);
+    if (filePath != null) {
+        return new File(filePath);
+    } else {
+        return null;
+    }
+}
+
+/**
+ * Sets the file path of the currently loaded file. The path is persisted in
+ * the OS specific registry.
+ * 
+ * @param file the file or null to remove the path
+ */
+public void setPersonFilePath(File file) {
+    Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+    if (file != null) {
+        prefs.put("filePath", file.getPath());
+
+        // Update the stage title.
+        primaryStage.setTitle("AddressApp - " + file.getName());
+    } else {
+        prefs.remove("filePath");
+
+        // Update the stage title.
+        primaryStage.setTitle("AddressApp");
+    }
+}
 
     /**
      * Returns the main stage
