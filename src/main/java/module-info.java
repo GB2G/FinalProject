06c421524d4638d005f0129a1ffe08cb5afe0e0c
GB2G <kev2005@outlook.com>
2022-06-14 22:47:45 -0400

    tried fixing problem still issue. Ask abt it tmrw

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module edu.kje.address {
     requires javafx.fxml;
     requires java.prefs;
     requires java.xml.bind;
+    requires transitive javafx.graphics;
 
     opens edu.kje.address to javafx.fxml;
     opens edu.kje.address.model to java.xml.bind;
